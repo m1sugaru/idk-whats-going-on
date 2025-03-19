68dc50db8e5d7d225caa752137daef4d79f9038b
m1sugaru <alexdifarnecio@gmail.com>
2025-03-19 19:18:44 +0900

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,4 @@ CREATE TABLE users (
 INSERT INTO users (id, name, age) VALUES (1, 'Alice', 25);
 INSERT INTO users (id, name, age) VALUES (2, 'Bob', 30);
 INSERT INTO users (id, name, age) VALUES (3, 'Charlie', 22);
+SELECT * FROM users;
